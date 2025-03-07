c248ae560904770609e31bbb14def4405479adb8
rnerer <rnerer@yahoo.com>
2025-03-07 21:42:43 +0100

    change in dev

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -10,6 +10,9 @@ sap.ui.define([
         return Controller.extend("com.systemfox.nr.pnr1.controller.View1", {
             onInit: function () {
 
+                // comment added in dev brancs to check merge
+                let y = 2;
+
             }
         });
     });
